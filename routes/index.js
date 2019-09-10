e884ab32ba6156a2514aaf33e69cac68df97dfde
Mvcampbell3 <mvcampbell3@gmail.com>
2019-09-10 13:20:44 -0400

    basic views routing

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get("/", (req, res) => {
   res.render("home")
 })
 
+router.get("/login", (req, res) => {
+  res.render("login");
+})
+
+router.get("/signup", (req, res) => {
+  res.render("signup")
+})
+
+router.get("/members", (req, res) => {
+  res.render("members")
+})
+
 module.exports = router;
