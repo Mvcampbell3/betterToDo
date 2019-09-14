d41d35bb1d19ba38f02e6aca769e342f77da9028
Mvcampbell3 <mvcampbell3@gmail.com>
2019-09-13 21:29:13 -0400

    small copy changes and added login alert function

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -12,19 +12,43 @@ logBtn.addEventListener("click", function(e) {
   if (email && password) {
     $.ajax("/api/user/login", {method: "POST", data: {email, password}})
       .then(result => {
-        console.log(result);
         // redirect to members page on result.success
         if (result.success) {
           window.location.replace("/members");
         }
       }).catch(err => {
+        let errorArray = [];
         console.log(err);
         console.log(err.responseText)
         // display to user err
         if (err.responseText === "Unauthorized") {
-          alert("Email and/or password is incorrect")
-
+          errorArray.push({msg: "Email and/or Password was incorrect"});
+        } else {
+          errorArray.push({msg: "Something went wrong"})
         }
+        addAlert(errorArray);
       })
   }
 })
+
+function addAlert(errors) {
+  if (errors.length === 0) {
+    errors.push({ msg: "Something went wrong" })
+  }
+  errors.forEach(one => {
+    const newAlert = document.createElement("div");
+    newAlert.classList = "alert alert-warning alert-dismissible fade show";
+    newAlert.setAttribute("role", "alert")
+    newAlert.textContent = one.msg;
+
+    const closeAlert = document.createElement("button");
+    closeAlert.classList = "close"
+    closeAlert.setAttribute("data-dismiss", "alert")
+    closeAlert.setAttribute("aria-label", "Close")
+    closeAlert.innerHTML = "<span aria-hidden='true'>&times;</span>"
+
+    newAlert.append(closeAlert);
+    const formAdd = document.getElementById("form");
+    formAdd.prepend(newAlert)
+  })
+}
